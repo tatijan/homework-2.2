930dae14da25947ee266b4eed750f543924428d1
tatijan <34741441+tatijan@users.noreply.github.com>
2018-02-15 09:00:53 +0200

    Add files via upload

--- list.php
+++ list.php
@@ -1,33 +1,33 @@
 <?php
-$allFiles = glob('tests/*.json');
+$file_list = glob('uploads/*.json');
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="styles/list.css">
+    <title>Список тестов</title>
 </head>
 <body>
-<a href="index.php" class="back"><div>< Назад</div></a>
-<hr>
-<?php if (!empty($allFiles)): ?>
-    <?php foreach ($allFiles as $file): ?>
 
-        <div class="file-block">
-            <h1><?php echo str_replace('tests/', '', $file); ?></h1><br>
-            <em>Загружен: <?php echo date("d-m-Y H:i", filemtime($file)) ?></em><br>
-            <a href="test.php?number=<?php echo array_search($file, $allFiles); ?>">Перейти на страницу с тестом ></a>
-        </div>
-        <hr>
+<?php
+foreach ($file_list as $key => $file) {
+    $file_test = file_get_contents($file);
+    $decode_file = json_decode($file_test, true);
+    //var_dump($decode_file);
+    foreach ($decode_file as $test) {
+        $question = $test['question'];
+        echo "<a href=\"test.php?test=$key\">$question</a><br>";
+    }
+}
+?>
 
-    <?php endforeach; ?>
-<?php endif; ?>
-<?php if (empty($allFiles)) echo 'Пока не загружено ни одного теста';?>
+<ul>
+    <li><a href="admin.php">Загрузить тест</a></li>
+    <li><a href="list.php">Список тестов</a></li>
+</ul>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script src="js/list.js"></script>
 </body>
 </html>

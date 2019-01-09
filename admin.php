1b45394fadd6ce22e29839c3bae4766daf2555ab
tatijan <34741441+tatijan@users.noreply.github.com>
2019-01-09 13:24:00 +0200

    Delete admin.php

--- admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set('display_errors','Off');
-if (empty(($_POST['add']))) {
-    if (empty($_POST['add'])) {
-        ?>
-        <p>Загрузите ваш тест - файл с расширением JSON</p>
-        <?php
